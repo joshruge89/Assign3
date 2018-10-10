57e665c60fab7aa358467420b19124623fe8d290
Josh <JoshRuge@gmail.com>
2018-10-10 08:31:17 -0500

    Upload3

--- Assign3/GradeForm.cs
+++ Assign3/GradeForm.cs
@@ -40,5 +40,40 @@ namespace Assign3
             CenterToScreen();
         }
 
+        private void ResultsButton1_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 1"; 
+        }
+
+        private void ResultsButton2_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 2";
+        }
+
+        private void ResultsButton3_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 3";
+        }
+
+        private void ResultsButton4_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 4";
+        }
+
+        private void ResultsButton5_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 5";
+        }
+
+        private void ResultsButton6_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 6";
+        }
     }
 }
