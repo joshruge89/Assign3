f4cd838646bfe0a850a1f19406399de07bc7f196
tradesdr <tysabs@gmail.com>
2018-10-10 19:25:28 -0500

    Add Form Controllers and Model. Improve layout

---
+++ Assign3/GradeForm.ButtonController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Assign3
+{
+    public partial class GradeForm
+    {
+
+        private void ResultsButton1_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 1";
+        }
+
+        private void ResultsButton2_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 2";
+        }
+
+        private void ResultsButton3_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 3";
+        }
+
+        private void ResultsButton4_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 4";
+        }
+
+        private void ResultsButton5_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 5";
+        }
+
+        private void ResultsButton6_Click(object sender, EventArgs e)
+        {
+            MainOutputBox.Clear();
+            MainOutputBox.Text = "Button 6";
+        }
+
+
+    }
+}
